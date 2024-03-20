4130c231e6a0b38c3e52be304b1ba5a0dc178ca1
gitName <a@b.c>
2024-03-20 13:03:45 +0800

    workload identity demo

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/zhangchl007/workload-identity-demo
+
+go 1.22.1
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.10.0
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v1.3.1
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.2
+	k8s.io/klog v1.0.0
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,106 @@
+// workload identity with Azure AD MSAL to access Azure Storage Account
+// This example demonstrates how to use Azure AD Workload Identity with MSAL to access an Azure Storage Account
+// The Azure AD Workload Identity webhook will inject the following environment variables:
+package main
+
+import (
+	"os"
+	"time"
+	"workload_identity_demo/pkg/azurerestmsal"
+
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"k8s.io/klog"
+)
+
+func main() {
+
+	// Azure Container
+
+	azure_storage_account_Name := os.Getenv("AZURE_STORAGE_ACCOUNT_NAME")
+
+	if azure_storage_account_Name == "" {
+		klog.Fatal("AZURE_STORAGE_ACCOUNT_NAME environment variable is not set")
+	}
+
+	azure_storage_account_Key := os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")
+
+	if azure_storage_account_Key == "" {
+		klog.Fatal("AZURE_STORAGE_ACCOUNT_KEY environment variable is not set")
+	}
+
+	pod_name := os.Getenv("POD_NAME")
+
+	if pod_name == "" {
+		klog.Fatal("POD_NAME environment variable is not set")
+	}
+
+	pod_ip := os.Getenv("POD_IP")
+
+	if pod_ip == "" {
+		klog.Fatal("POD_IP environment variable is not set")
+
+	}
+
+	// Azure AD Workload Identity webhook will inject the following env vars
+	// 	AZURE_CLIENT_ID with the clientID set in the service account annotation
+	// 	AZURE_TENANT_ID with the tenantID set in the service account annotation. If not defined, then
+	// 	the tenantID provided via azure-wi-webhook-config for the webhook will be used.
+	// 	AZURE_FEDERATED_TOKEN_FILE is the service account token path
+	// 	AZURE_AUTHORITY_HOST is the AAD authority hostname
+	clientID := os.Getenv("AZURE_CLIENT_ID")
+	tenantID := os.Getenv("AZURE_TENANT_ID")
+	tokenFilePath := os.Getenv("AZURE_FEDERATED_TOKEN_FILE")
+	authorityHost := os.Getenv("AZURE_AUTHORITY_HOST")
+
+	if clientID == "" {
+		klog.Fatal("AZURE_CLIENT_ID environment variable is not set")
+	}
+	if tenantID == "" {
+		klog.Fatal("AZURE_TENANT_ID environment variable is not set")
+	}
+
+	if tokenFilePath == "" {
+		klog.Fatal("AZURE_FEDERATED_TOKEN_FILE environment variable is not set")
+	}
+
+	if authorityHost == "" {
+		klog.Fatal("AZURE_AUTHORITY_HOST environment variable is not set")
+	}
+
+	//
+
+	cred, err := azurerestmsal.NewAzureMSALTokenCredential(context.Background(), clientID, tenantID, tokenFilePath, authorityHost)
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	storageaccountURL := "https://" + azure_storage_account_Name + ".blob.core.windows.net"
+	containerName := "pvc-7caab612-5410-4125-9db4-51fec72610de"
+	blobName := "test.txt"
+	ctx := context.Background()
+
+	mydata := time.Now().Format(time.RFC3339) + pod_name + pod_ip + "Hello, World!"
+
+	data := []byte(mydata)
+
+	// Create a new Azure Storage Account client
+	//containerURL := "https://" + azure_storage_account_Name + ".blob.core.windows.net/" + "pvc-7caab612-5410-4125-9db4-51fec72610de"
+
+	// Create a new container client
+
+	client, err := azblob.NewClient(storageaccountURL, cred, nil)
+
+	if err != nil {
+		klog.Fatal(err)
+
+	}
+
+	// write a buffer to a blob
+	_, err = client.UploadBuffer(ctx, containerName, blobName, data, &azblob.UploadBufferOptions{})
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+}
